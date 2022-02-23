f3e72d658fc0f56eb89784524d82da427c273d42
Ben <si_ben_tran@hotmail.com>
2022-02-23 09:51:02 +0100

    add closures map

--- Funktionales_Programmieren/Closures_map.py
+++ Funktionales_Programmieren/Closures_map.py
@@ -1,12 +1,16 @@
 from typing import Callable
 
+
 def mal(x: int) -> Callable[[int], int]:
     def mal_x(y: int) -> int:
         return x * y
+
     return mal_x
 
-def multipliziere_liste_funktional(lst: list, factor: int) -> list:
-    mal_factor = mal(factor)
-    return list(map(lst, mal_factor))
 
-print(multipliziere_liste_funktional([10,20],2))
+def multipliziere_liste_funktional(lst: list, k: int) -> list:
+    mal_k = mal(k)
+    print(list(map(mal_k, lst)))
+
+
+multipliziere_liste_funktional([10, 20], 5)
