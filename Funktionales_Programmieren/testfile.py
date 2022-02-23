285cc2af15e076aa9c7a4ba18e91e4e44d494b70
Ben <si_ben_tran@hotmail.com>
2022-02-23 09:51:09 +0100

    add testfile

--- Funktionales_Programmieren/testfile.py
+++ Funktionales_Programmieren/testfile.py
@@ -1,12 +1,16 @@
 from typing import Callable
 
-def mal(x: int) -> Callable[[int],int]:
-	def mal_x(y: int) -> int:
-		return x * y
-	return mal_x
+
+def mal(x: int) -> Callable[[int], int]:
+    def mal_x(y: int) -> int:
+        return x * y
+
+    return mal_x
+
 
 def multipliziere_liste_funktional(xs: list, k: int) -> list:
-	mal_k = mal(k)
-	print(list(map(mal_k, xs)))
+    mal_k = mal(k)
+    print(list(map(mal_k, xs)))
 
-multipliziere_liste_funktional([10,20],2)
+
+multipliziere_liste_funktional([10, 20], 2)
