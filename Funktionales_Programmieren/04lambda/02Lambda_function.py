da699e381bc48c58a926b06a37b0c24813d9a9c3
Ben <si_ben_tran@hotmail.com>
2022-04-12 15:28:14 +0200

    add lambda stuff

---
+++ Funktionales_Programmieren/04lambda/02Lambda_function.py
@@ -0,0 +1,9 @@
+def quadrat_umfang(seitenlaenge: int) -> int:
+    return seitenlaenge * 4
+
+result1 = quadrat_umfang(10)
+
+# Lambda ist eine Anonyme Funktion die kein Namenszuweisung benoetigt
+result2 = (lambda x : x * 4)(10)
+print(result1, result2)
+
